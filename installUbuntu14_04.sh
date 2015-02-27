f3e20c646f8653e86003e67472d4e1b9d2663b93
Byran <bryanroscoe@gmail.com>
2015-02-27 23:18:53 +0000

    Fixed type in file name

--- installUbuntu14_04.sh
+++ installUbuntu14_04.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 \curl -sSL https://raw.githubusercontent.com/bryanroscoe/ipset-blacklist/master/update-blacklist.sh > /etc/cron.daily/update-blacklist.sh
 chmod +x /etc/cron.daily/update-blacklist.sh
-\curl -sSL https://raw.githubusercontent.com/bryanroscoe/ipset-blacklist/master/blacklistInit >  /etc/network/if-pre-up.d/blocklistInit
-chmod +x /etc/network/if-pre-up.d/blocklistInit
+\curl -sSL https://raw.githubusercontent.com/bryanroscoe/ipset-blacklist/master/blacklistInit >  /etc/network/if-pre-up.d/blacklistInit
+chmod +x /etc/network/if-pre-up.d/blacklistInit
 apt-get install -y ipset
 ipset create blacklist hash:net
 /etc/cron.daily/update-blacklist.sh
